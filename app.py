344da3c8166df04f13fbe7ebc205efd38c5b6d2e
peas8810 <127704090+peas8810@users.noreply.github.com>
2025-05-05 18:55:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -136,7 +136,12 @@ def generate_pdf_report(results: dict) -> str:
         " - Uso de conectores: expressões como 'Portanto', 'Além disso' são frequentes.\n"
         " - Frases genéricas: construção sofisticada, porém superficial.\n"
         " - Padrões linguísticos: falta de nuances humanas (ironias, ambiguidade).\n\n"
-        "Interpretação do valor:\n"
+        
+        "Interpretação do valor - Entropia:\n"
+        "0% - 3%    Alta probabilidade de IA (muito provável que o texto seja gerado por um modelo de linguagem como GPT, Bard, etc.)\n"
+        "3% - 6%    Baixa probabilidade de IA (provavelmente texto humano)\n"
+        
+        "Interpretação do valor - Roberta:\n"
         "0% - 30%    Baixa probabilidade de IA (provavelmente texto humano)\n"
         "30% - 60%   Área de incerteza (o texto pode conter partes geradas por IA ou apenas seguir um padrão formal)\n"
         "60% - 100%  Alta probabilidade de IA (muito provável que o texto seja gerado por um modelo de linguagem como GPT, Bard, etc.)"
